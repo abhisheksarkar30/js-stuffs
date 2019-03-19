2ebd839b558dc4bbf1c91357d08917a17ac8c829
Abhishek Sarkar <abhisheksarkar30@gmail.com>
2019-03-19 19:56:15 +0530

    Bugfix v1: Redirect only from Google search

--- URLRedirectOnRequest/src/content.js
+++ URLRedirectOnRequest/src/content.js
@@ -1,12 +1,14 @@
-var pattern1=/^thagm/i;
-var pattern2=/^thagmo/i;
-var pattern3=/^cuslone/i;
+var patterns=["thagm","thagmo","cuslone"];
 var viewtext_base_url = "http://jira.nrifintech.com/browse/";
 var newurl;
-var currentTitle = window.document.title;
+var currentTitle = window.document.title.toLowerCase();
 
-if (pattern1.test(currentTitle) || pattern2.test(currentTitle) || pattern3.test(currentTitle)) // if it matches pattern defined above
-{
-  newurl = viewtext_base_url + currentTitle.substring(0,currentTitle.indexOf(" "));
-  chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
+if(currentTitle.endsWith("google search")) {
+	patterns.forEach(function(currentPattern) {
+		if (currentTitle.startsWith(currentPattern)) // if it matches pattern defined above
+		{  
+			newurl = viewtext_base_url + currentTitle.substring(0,currentTitle.indexOf(" "));
+			chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
+		}
+	});
 }
